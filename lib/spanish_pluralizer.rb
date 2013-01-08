374be735148912a9d9dd8cf1ea389b77267fbecc
Pablo <bianciottopablo@gmail.com>
2013-01-07 21:26:10 -0300

    Refactor and DRY up of acute, unacute, is_acute?

--- lib/spanish_pluralizer.rb
+++ lib/spanish_pluralizer.rb
@@ -5,94 +5,75 @@ end
 
 class String
 
-  raise AlreadyDefinedMethod.new unless (self.instance_methods(true) & [:acute, :unacute, :pluralize_spanish, :is_acute?]).empty?
+  clashing_methods = self.instance_methods(true) & [:pluralize_spanish, :spanish_pluralizer_acute, :spanish_pluralizer_unacute, :spanish_pluralizer_is_acute?]
+  raise AlreadyDefinedMethod.new("The following instance methods for String have already been defined: #{clashing_methods * ', '}. Are you calling load more than once?") unless clashing_methods.empty?
 
-  def acute
-    case self
-      when 'a'
-        'á'
-      when 'e'
-        'é'
-      when 'i'
-        'í'
-      when 'o'
-        'ó'
-      when 'u'
-        'ú'
-      else self
-    end
+  SPANISH_PLURALIZER_ACUTES = {
+    'a' => 'á',
+    'e' => 'é',
+    'i' => 'í',
+    'o' => 'ó',
+    'u' => 'ú'
+  }
+
+  def spanish_pluralizer_acute
+    SPANISH_PLURALIZER_ACUTES.fetch(self, self)
   end
 
-  def unacute
-    case self
-      when 'á'
-        'a'
-      when 'é'
-        'e'
-      when 'í'
-        'i'
-      when 'ó'
-        'o'
-      when 'ú'
-        'u'
-      else self
-    end
+  def spanish_pluralizer_unacute
+    SPANISH_PLURALIZER_ACUTES.invert.fetch(self, self)
   end
 
-  def is_acute?
-    ['á','é','í','ó','ú'].include? self
+  def spanish_pluralizer_is_acute?
+    SPANISH_PLURALIZER_ACUTES.values.include? self
   end
 
   def pluralize_spanish
     #orden por probabilidad de ocurrencia para optimizar
     case self
-      when /.*[aeiouáéó]$/
-        #caja => cajas
-        #puente => puentes
-        #papi => papis
-        #mono => monos
-        #río => ríos
-        #palabru => palabrus
-        #sofá => sofás
-        #palabré => palabrés
-        #palabró => palabrós
-        self + 's'
-      when /(?<pre>.*)(?<last_vowel>.)(?<ending>[ns])$/
-        if $~[:last_vowel].is_acute?
-          new_str = self
-          #aguda con tilde => grave sin tilde, a menos que el tilde se mantenga por existir hiato
-          new_str[-2] = new_str[-2].unacute unless new_str[-3..-1].match(/(a|e|o)(í|ú)/)
-          new_str + 'es'
-        else
-          if $~[:ending] == 's'
-            #análisis => análisis
-            self
-          else
-            #imagen => imágenes
-            #palebren => palébrenes
-            #origen => orígenes
-            #orden => órdenes
-            #resumen => resúmenes
-            ending = $~[:last_vowel] + 'nes'
-            $~[:pre].sub(/(a|e|i|o|u)(?=[^aeiou]*$)/, &:acute) + ending
-          end
-        end
-      when /.*z$/
-        #arroz => arroces
-        self[0..-2] + 'ces'
+    #caja => cajas
+    #puente => puentes
+    #papi => papis
+    #mono => monos
+    #río => ríos
+    #palabru => palabrus
+    #sofá => sofás
+    #palabré => palabrés
+    #palabró => palabrós
+    when /.*[aeiouáéó]$/ then self + 's'
+    when /(?<pre>.*)(?<last_vowel>.)(?<ending>[ns])$/
+      if $~[:last_vowel].spanish_pluralizer_is_acute?
+        new_str = self
+        #aguda con tilde => grave sin tilde, a menos que el tilde se mantenga por existir hiato
+        new_str[-2] = new_str[-2].spanish_pluralizer_unacute unless new_str[-3..-1].match(/(a|e|o)(í|ú)/)
+        new_str + 'es'
+      elsif $~[:ending] == 's'
+        #análisis => análisis
+        self
       else
-        #delantal => delantales
-        #mantel => manteles
-        #mandril => mandriles
-        #tambor => tambores
-        #gandul => gandules
-        #maní => maníes
-        #menú => menúes
-        #rey => reyes
-        #fax => faxes
-        #árbol => árboles
-        #laúd => laúdes
-        self + 'es'
+        #imagen => imágenes
+        #palebren => palébrenes
+        #origen => orígenes
+        #orden => órdenes
+        #resumen => resúmenes
+        ending = $~[:last_vowel] + 'nes'
+        $~[:pre].sub(/(a|e|i|o|u)(?=[^aeiou]*$)/, &:spanish_pluralizer_acute) + ending
+      end
+    #arroz => arroces
+    when /.*z$/ then self[0..-2] + 'ces'
+    #delantal => delantales
+    #mantel => manteles
+    #mandril => mandriles
+    #tambor => tambores
+    #gandul => gandules
+    #maní => maníes
+    #menú => menúes
+    #rey => reyes
+    #fax => faxes
+    #árbol => árboles
+    #laúd => laúdes
+    else
+     self + 'es'
     end
   end
 end
